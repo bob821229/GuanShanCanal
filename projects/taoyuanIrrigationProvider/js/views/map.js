fa5253861e0122d86740bd6f78352b33496f7649
JohnnyChen(Mac) <jc.freelance.2011@gmail.com>
2024-08-12 20:44:19 +0800

    地圖篩選

--- projects/taoyuanIrrigationProvider/js/views/map.js
+++ projects/taoyuanIrrigationProvider/js/views/map.js
@@ -19,7 +19,7 @@ export default {
             pondProfile: {
                 hierarchyList: null,
                 pondInfoList: null,
-                workstationGroupList: null, 
+                workstationGroupList: null,
             },
             dataAccess: null
         }
@@ -32,9 +32,12 @@ export default {
         }
     },
     computed: {
-        workstationGroupListData(){
+        mapBaseQuery() {
+            return `管理處名稱 = '${this.mapProfile.search.association}'`;
+        },
+        workstationGroupListData() {
 
-        }, 
+        },
         associationList() {
             if (this.pondProfile.hierarchyList == null) {
                 return [];
@@ -112,7 +115,7 @@ export default {
 
             }
 
-            let where = `管理處名稱 = '${this.mapProfile.search.association}'`;
+            let where = this.mapBaseQuery;//`管理處名稱 = '${this.mapProfile.search.association}'`;
             console.log(where);
             this.mapProfile.layer = L.esri
                 .dynamicMapLayer({
@@ -120,29 +123,34 @@ export default {
                     opacity: 0.7,
                     layers: [
                         10, //BB_4水利小組範圍_桃管_石管
-                        11, //BB_5圳路渠道_桃管_石管_幹支線
+                        //11, //BB_5圳路渠道_桃管_石管_幹支線
                         13, //BB_3工作站範圍_桃管_石管
                         14, //BB_6埤塘_1120512_桃管_石管
                         15//, //BB_1管理處範圍_桃管_石管
                         //12, //BB_5圳路渠道_桃管_石管_所有渠道
-                    ], 
-                    // layerDefs: {
-                    //     10: "0 = 0", //"管理處名稱 = '桃園管理處' and 工作站名稱 = '湖口工作站'", //'0=0', 
-                    //     11: where, 
-                    //     13: where, 
-                    //     14: where, 
-                    //     15: where, 
-                        
-                    // }
+                    ],
+                    layerDefs: {
+                        10: where,
+                            //"管理處名稱 = '桃園管理處' and 工作站名稱 = '湖口工作站'", 
+                            //'0=0',
+                        //11: where,
+                        13: where,
+                        14: where,//`管理處名稱 = '桃園管理處' and 工作站名稱 = '湖口工作站' and 水利小組名稱 = '光復圳1-1號池小組'`, //where,
+                        15: where,
+
+                    }
                 })
                 .addTo(this.mapProfile.map);
 
-                
+
 
             this.mapProfile.layer
                 .query()
                 .layer(14)
-                .where(where)
+                .where(
+                    where
+                    //`管理處名稱 = '桃園管理處' and 工作站名稱 = '湖口工作站' and 水利小組名稱 = '光復圳1-1號池小組'`
+                )
                 .bounds((error, latlngbounds) => {
                     if (error) {
                         console.error("Error querying feature layer bounds:", error);
@@ -159,15 +167,84 @@ export default {
                     this.mapProfile.map.fitBounds(latlngbounds);
                 });
 
-            this.mapProfile.layer.bindPopup(function (err, featureCollection, response) {
+            this.mapProfile.layer.bindPopup((err, featureCollection, response) => {
                 console.log('dynamicMapLayer.bindPopup');
+
+                let cont = '';
+
+                let workstation = null;
+                let pondName = null;
                 featureCollection.features.forEach(f => {
                     console.log(f);
+                    cont += this.getPondContent(f.properties);
+                    if (f.layerId == 14) {
+                        // let obj = f.properties;
+                        workstation = f.properties['工作站名稱'];
+                        pondName = f.properties['埤塘名稱'];
+                    }
                 });
-                var count = featureCollection.features.length;
-                return (count) ? count + ' features' : false;
+                // var count = featureCollection.features.length;
+                // //return (count) ? count + ' features' : false;
+                // return `<div style="background-color: blue;">${count} f</div>`;
+                cont += this.getDep1DataContent(pondName, workstation);
+                return cont;
             });
         },
+        getPondContent: function (gisProperties) {
+            let cont = '';
+            Object.keys(gisProperties).forEach(key => {
+                //console.log(`Key: ${key}, Value: ${obj[key]}`);
+                let value = gisProperties[key];
+                cont += `<div class="col-md-6">
+                    <label>${key}: </label>
+                    <span>${value}</span>
+                </div>`
+            });
+            
+            // if(cont != ''){
+            //     cont = `<div class="row">${cont}</div>`;
+            // }
+            cont += '<hr class="col-md-12">'
+            return cont;
+        }, 
+        getDep1DataContent: function(pondName, workstation){
+            let cont = '';
+            if(pondName != null){
+                let found = Enumerable.from(this.pondProfile.pondInfoList)
+                    .where(item => {return item['埤塘名稱'] == pondName && item['工作站'] == workstation;})
+                    .toArray();
+                console.log('found ponds: ', found);
+                if(found.length > 0){
+                    // cont += `<hr class="col-md-12">`;
+
+                    let dataObject = found[0];
+                    Object.keys(dataObject).forEach(key => {
+                        //console.log(`Key: ${key}, Value: ${obj[key]}`);
+                        let value = dataObject[key];
+                        cont += `<div class="col-md-6">
+                            <label>${key}: </label>
+                            <span>${value}</span>
+                        </div>`
+                    });
+                }
+            }
+            return cont;
+        }, 
+        getWhereQuery: function(whereString, searchField, gisField){
+            let r = '';
+
+            let conditionValue = this.mapProfile.search[searchField];
+            if(conditionValue != null && conditionValue.length > 0){
+                if(whereString.length > 0){
+                    r = `${whereString} and ${gisField} = '${conditionValue}'`;
+                }else{
+                    r = `${gisField} = '${conditionValue}'`
+                }
+            }else{
+                r = whereString;
+            }
+            return r;
+        }, 
         // testMap: function () {
         //     let url = 'https://gisportal.triwra.org.tw/server/rest/services/BigBossTaoyuanPonds2/MapServer';
         //     this.mapProfile.map = L.map("map").setView(
@@ -227,38 +304,45 @@ export default {
         //         return (count) ? count + ' features' : false;
         //     });
         // },
-        // searchMap: function(){
-        //     let where = `工作站名稱 = '桃園工作站' and 水利小組名稱 = '1-4號池小組'`;
-        //     this.mapProfile.layer.setLayerDefs(
-        //         {
-        //             // 10: where, 
-        //             // 11: where, 
-        //             // 13: where, 
-        //             14: where, 
-        //             //15: where, 
-                    
-        //         }
-        //     );
-        //     this.mapProfile.layer
-        //         .query()
-        //         .layer(14)
-        //         .where(where)
-        //         .bounds((error, latlngbounds) => {
-        //             if (error) {
-        //                 console.error("Error querying feature layer bounds:", error);
-        //                 alert('查無資料');
-        //                 return;
-        //             }
+        searchMap: function () {
+            let where = '';
+            where = this.getWhereQuery(where, 'association', '管理處名稱');
+            where = this.getWhereQuery(where, 'workstation', '工作站名稱');
+            where = this.getWhereQuery(where, 'group', '水利小組名稱');
+            //`管理處名稱 = '桃園管理處' and 工作站名稱 = '桃園工作站'`;
+            console.log(where);
+            this.mapProfile.layer.setLayerDefs(
+                {
+                    10: where, 
+                    //11: where, 
+                    13: where, 
+                    14: where,
+                    15: where, 
+                }
+            );
+            // setTimeout(() => {
+            //     let q = this.mapProfile.layer
+            //         .query()
+            //         .layer(14)
+            //         .where(where);
+            //     q.bounds((error, latlngbounds) => {
+            //         if (error) {
+            //             console.error("Error querying feature layer bounds:", error);
+            //             alert('查無資料');
+            //             return;
+            //         }
 
-        //             if (latlngbounds._northEast == null) {
-        //                 alert('查無資料');
-        //                 return;
-        //             }
-        //             //this.ifFeatureLayerQuery = true;
-        //             // Fit the map to the bounds of the features
-        //             this.mapProfile.map.fitBounds(latlngbounds);
-        //         });
-        // }, 
+            //         if (latlngbounds._northEast == null) {
+            //             alert('查無資料');
+            //             return;
+            //         }
+            //         //this.ifFeatureLayerQuery = true;
+            //         // Fit the map to the bounds of the features
+            //         this.mapProfile.map.fitBounds(latlngbounds);
+            //     });
+            // }, 500);
+            
+        },
     },
     mounted() {
         this.init();
@@ -289,7 +373,7 @@ export default {
                             <option selected value="">不篩選</option>
                             <option v-for="(obj, idx) in groupList" :value="obj">{{obj}}</option>
                         </select>
-                        <button class="btn btn-outline-secondary" type="button">查詢</button>
+                        <button class="btn btn-outline-secondary" type="button" @click="searchMap">查詢</button>
                     </div>
                 </div>
                 
